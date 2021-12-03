c126a852420a1eae770085abd37c3461bec73b04
Brice Briçou <briceorl54580@gmail.com>
2021-12-03 09:23:26 +0100

    Update : rassemblement de fichier & formatage

--- Code_donnee_au_debut/main.c
+++ Code_donnee_au_debut/main.c
@@ -1,3 +1,19 @@
+/* ***** ***** ***** main2 ***** ***** ***** */
+const char mode = argv[1][0];
+const bool utility = (atoi(argv[2]) == 0) ? false : true;
+const int Vmax = atoi(argv[3]);
+
+struct retained_t *retained_objects = new_bag();
+struct objects_t *object_set = new_objects(argc, argv, utility);
+
+#ifdef _TRACE_
+fprintf(stderr, "mode=%c usefullness=%c Vmax=%d\n", mode, (utility)?'U':'N', Vmax);
+fprintf(stderr, "Object set full with %d objects\n", object_set->nb_objects);
+view_objet_set(object_set);
+#endif
+/* ***** ***** ***** ***** ***** ***** ***** */
+
+
 int main(int argc, char **argv) {
     /** @brief main parameters are :
       * - argc : # of parameters
@@ -14,3 +30,18 @@ int main(int argc, char **argv) {
         fprintf(stderr, "\t* use_i is for #i object's utility, i in {1, ... , n}");
         exit(-1);
     }
+
+    /* ***** main2 ***** */
+    if(mode == 'R') {
+        struct retained_t *my_bag = new_bag();
+        prec(Vmax, object_set, my_bag);
+        view_bagpack(my_bag, "Final selection");
+    }
+    else if(mode == 'A') {
+        dp_array(Vmax, object_set);
+    } else {
+        dp_list(Vmax, object_set);
+    }
+
+    return EXIT_SUCCESS;
+}
--- Code_donnee_au_debut/rec/objects.c
+++ Code_donnee_au_debut/rec/objects.c
@@ -14,3 +14,13 @@ struct objects_t *new_objects(int argc, char **argv, bool utility) {
         }
     return set;
 }
+
+void view_object(struct object_t *object) {
+    printf("(%d , %d) ", object->volume, object->utility);
+}
+
+void view_objet_set(struct objects_t *set) {
+    printf("\n*** View objet set ***\n");
+    for (int o = 0; o < set->nb_objects; o += 1) view_object(set->objects + o);
+    printf("\n**********************\n");
+}
