932b60a890e0f400dfc7fadf07ebedc79899390e
Brice Briçou <briceorl54580@gmail.com>
2021-12-29 15:23:51 +0100

    Add : cmake-build-debug

---
+++ cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/bag.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/bag.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/dp_array.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/dp_array.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/dp_list.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/dp_list.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/dp_rec.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/dp_rec.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/elmlist.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/elmlist.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/list.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/list.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/main.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/main.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/objects.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/objects.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/outils.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/outils.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/space_array.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/space_array.c.obj"
+  "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/src/states.c" "D:/W_Programmation/Licence_Info/S3/Prog_Avance/Projet_ProgAv-S3/cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/src/states.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Projet_ProgAv_S3.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/bag.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/dp_array.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/dp_list.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/dp_rec.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/elmlist.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/list.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/main.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/objects.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/outils.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/space_array.c.obj"
+  "CMakeFiles/Projet_ProgAv_S3.dir/src/states.c.obj"
+  "Projet_ProgAv_S3.exe"
+  "Projet_ProgAv_S3.exe.manifest"
+  "Projet_ProgAv_S3.lib"
+  "Projet_ProgAv_S3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_ProgAv_S3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
