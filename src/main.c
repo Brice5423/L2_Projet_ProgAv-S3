e947b213d12b7c45450124d80eb0c66933100373
Brice Briçou <briceorl54580@gmail.com>
2021-12-29 15:54:44 +0100

    Update : main.c (Vmax -> VMax)

--- src/main.c
+++ src/main.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv) {
 
     const char mode = argv[1][0];
     const bool utility = (atoi(argv[2]) == 0) ? false : true;
-    const int Vmax = atoi(argv[3]);
+    const int VMax = atoi(argv[3]);
 
     struct retained_t *retained_objects;
     struct objects_t *object_set;
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
     object_set = new_objects(argc, argv, utility);
 
     #ifdef _TRACE_
-        fprintf(stderr, "mode=%c usefullness=%c Vmax=%d\n", mode, (utility) ? 'U' : 'N', Vmax);
+        fprintf(stderr, "mode=%c usefullness=%c Vmax=%d\n", mode, (utility) ? 'U' : 'N', VMax);
         fprintf(stderr, "Object set full with %d objects\n", object_set->nb_objects);
         view_objet_set(object_set);
     #endif
@@ -43,12 +43,12 @@ int main(int argc, char **argv) {
         struct retained_t *my_bag;
 
         my_bag = new_bag();
-        prec(Vmax, object_set, my_bag);
+        prec(VMax, object_set, my_bag);
         view_bagpack(my_bag, "Final selection");
     } else if (mode == 'A') {
-        dp_array(Vmax, object_set);
+        dp_array(VMax, object_set);
     } else {
-        dp_list(Vmax, object_set);
+        dp_list(VMax, object_set);
     }
 
     return EXIT_SUCCESS;
